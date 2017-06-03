4e9091f93a043ef2cf8cb60d4cedbf433e0770db
R.Brown <ready.brown@hotmail.de>
2017-06-03 17:38:34 +0200

    - update build script

--- dockerfiles/bin/build.sh
+++ dockerfiles/bin/build.sh
@@ -3,18 +3,17 @@
 # Copy environment config
 cp .env.example .env
 
-# Install dependencies
-composer install --prefer-dist --no-interaction
-
-# Install dependencies
-#nvm install $NODEJS_VERSION
-npm install
-
 # Generate application key
 php artisan key:generate
 
 # Verify environment config
 cat .env
+
+# Install dependencies
+composer install --prefer-dist --no-interaction
+
+# Install dependencies
+npm install
 
 # Install dependencies
 bower install --allow-root
