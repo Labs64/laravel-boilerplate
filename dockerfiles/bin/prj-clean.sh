015120211049bb5eb76f01d6ab325453198d1c6d
R.Brown <ready.brown@hotmail.de>
2017-06-04 14:07:13 +0200

    - cleanup

--- dockerfiles/bin/prj-clean.sh
+++ dockerfiles/bin/prj-clean.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#rm -Rf bootstrap/cache/
 rm -Rf data/
 rm -Rf bower_components/
 rm -Rf node_modules/
 rm -Rf vendor/
-rm composer.lock
 rm -Rf public/assets
-rm public/mix-manifest.json
