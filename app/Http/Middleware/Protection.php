55c8f919a015e213fb7a6a93923662a6fd353b3f
Slava Rudkovskiy <xraskolx@gmail.com>
2017-10-23 10:42:00 +0300

    Issue #29 Handle invalid NetLicensing credentials

--- app/Http/Middleware/Protection.php
+++ app/Http/Middleware/Protection.php
@@ -4,6 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use NetLicensing\Context;
+use NetLicensing\NetLicensingService;
+use NetLicensing\RestException;
 
 class Protection
 {
@@ -23,11 +26,31 @@ class Protection
             return redirect()->route('login');
         }
 
-        if (!auth()->user()->hasAccess($productModuleNumber)){
-            return redirect()->route($failedRouteName, [
-                'dest' => url()->current(),
-                'product_module_number' => $productModuleNumber,
-            ]);
+        try {
+            if (!auth()->user()->hasAccess($productModuleNumber)) {
+                return redirect()->route($failedRouteName, [
+                    'dest' => url()->current(),
+                    'product_module_number' => $productModuleNumber,
+                ]);
+            }
+        } catch (\Exception $e) {
+            if ($e instanceof RestException) {
+
+                $wiki = 'Check out our wiki https://github.com/Labs64/laravel-boilerplate/wiki/NetLicensing-connection-error';
+
+                if (NetLicensingService::getInstance()->lastCurlInfo()->httpStatusCode == 401) {
+                    throw new RestException($e->getMessage() . ' ' . $wiki);
+                }
+
+                switch (env('LABS64_NETLICENSING_SECURITY_MODE')) {
+                    case Context::BASIC_AUTHENTICATION:
+                        throw new RestException('Missing or invalid Username/Password ' . $wiki);
+                        break;
+                    case Context::APIKEY_IDENTIFICATION:
+                        throw new RestException('Missing or invalid API Key ' . $wiki);
+                        break;
+                }
+            }
         }
 
         return $next($request);
