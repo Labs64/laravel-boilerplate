90e11c006ad81dd450cb0e529521c3649c16625c
Slava Rudkovskiy <xraskolx@gmail.com>
2018-05-09 12:28:46 +0300

    Issue #42 Add function whose translates message based on a count from json key

--- app/helpers.php
+++ app/helpers.php
@@ -121,3 +121,19 @@ if (!function_exists('protection_validate')) {
         return app('netlicensing')->validate($user);
     }
 }
+
+if (!function_exists('__choice')) {
+    /**
+     * Translates the given message based on a count from json key.
+     *
+     * @param $key
+     * @param $number
+     * @param array $replace
+     * @param null $locale
+     * @return string
+     */
+    function __choice($key, $number, array $replace = [], $locale = null)
+    {
+        return trans_choice(__($key), $number, $replace, $locale);
+    }
+}
