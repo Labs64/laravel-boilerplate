7907e7b73832b33baa9517dbeb8135d8b9b8ea96
Rainer Kent Renegado <rainerkent.renegado@1722itservices.com>
2019-10-07 11:11:39 +0800

    Update Laravel to 6.x

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,7 +59,7 @@ return [
             'driver' => 's3',
             'key' => env('AWS_KEY'),
             'secret' => env('AWS_SECRET'),
-            'region' => env('AWS_REGION'),
+            'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
         ],
 
